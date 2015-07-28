3caf74485c7858817ee67116c2190a78aede2f50
ahmetyigityol <ayigitd@gmail.com>
2015-07-28 17:47:20 +0300

    main hello cleaned, idList added

--- cloud/main.js
+++ cloud/main.js
@@ -1,30 +1,6 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  Parse.Cloud.useMasterKey();
-  var emp = Parse.Object.extend("EMPLOYEE");
-  var query = new Parse.Query(emp);
-  query.limit(300);
-  query.find({
-		success: function(results) {
-			for (var i = 0; i < results.length; i++) {
-			  var object = results[i];
-			  if(Math.round(Math.random()*10000000000)%2){
-				object.set("GENDER","F");
-			  }
-			  else{
-				object.set("GENDER","M");
-			  }
-			  object.save();
-			}
-	  },
-	  error: function(error) {
-		console.log("yanlis");
-		response.success("not OK!");
-	  }
-	});
-});
 
 function errormsg(code, msg){
 	
@@ -90,35 +66,16 @@ Parse.Cloud.define("idList", function(request, response) {
 		var query = new Parse.Query(emp);
 		query.limit(300);
 		query.find({success: function(results){
-			if(results.length != 1){
-				response.success(errormsg("1","ID or Password is wrong."));
+				arr = [];
+				id = new Object();
+				for(i = 0; i<results.length; i++){
+					arr.push(new Object(results[i].get("ID")));
+				}
+				response.success(JSON.stringify(arr));
+				
+			},error: function(error) {
+				response.success(errormsg("3","Query error happened."));
 			}
-			else{
-					if((results[0].get("PASSWORD") == request.params["PASSWORD"])){
-						var person = new Object();
-						person["NAME"] = results[0].get("NAME");
-						person["SURNAME"] = results[0].get("SURNAME");
-						person["EMAIL"] = results[0].get("EMAIL");
-						person["ID"] = results[0].get("ID");
-						person["GENDER"] = results[0].get("GENDER");
-						person["RANK"] = results[0].get("RANK");
-						person["PIC"] = results[0].get("PIC");
-						person["CODE"] = "OK";
-						
-						
-						response.success(JSON.stringify(person));
-						
-					}
-					else{
-						response.success(errormsg("1","ID or Password is wrong."));
-					}
-			}
-			
-		},error: function(error) {
-			response.success(errormsg("3","Query error happened."));
-		}
-		
-		
 		});
 	
 });
